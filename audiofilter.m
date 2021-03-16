95ad9aca9b130a0d2b3b84307d94a0e8070f46c4
Bryn Davies <bryn.davies@sam.math.ethz.ch>
2021-03-16 10:42:39 +0100

    speed up achieved by performing convolution with fft/ifft

--- audiofilter.m
+++ audiofilter.m
@@ -3,7 +3,7 @@ function [a,Fs] = audiofilter(file,res,V)
 % Fs, due to a subwavelength scattering transform with resonant properties
 % given by res and V
 
-[x1,Fs] = audioread(file);
+[x1,Fs] = audioread(file);      % get the data and sampling rate for the audio file
 x = x1(1:Fs);
 
 T = 1/Fs;                     % Sampling period
@@ -11,10 +11,17 @@ L = length(x);                % Length of signal
 t = (0:L-1)*T;                % Time vector for signal
 N = length(res);              % Number of resonators
 
-%% Apply k-th band-pass filter
-a = zeros(N,L-1);
 for n = 1:N
-    for k = 1:L-1
-        a(n,k) = dot( h(t-t(k),n,res,V) , x );        % filtered signal
-    end
+    % Define the filter kernel
+    h = exp(imag(res(n))*t).*cos(real(res(n)*t));
+    % Compute the convolution using the fft/ifft
+    a(n,:) = fconv(x, h);
 end
+
+%% Apply k-th band-pass filter
+% a = zeros(N,L-1);
+% for n = 1:N
+%     for k = 1:L-1
+%         a(n,k) = dot( h(t-t(k),n,res,V) , x );        % filtered signal
+%     end
+% end
